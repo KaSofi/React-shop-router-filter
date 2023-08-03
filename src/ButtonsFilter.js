556af1f06c314b7b5bb275e40fad2715d6dbccbd
Mila Gvozdeva <103929930+KaSofi@users.noreply.github.com>
2023-08-03 14:38:08 +0300

    Update ButtonsFilter.js

--- src/ButtonsFilter.js
+++ src/ButtonsFilter.js
@@ -3,7 +3,7 @@ import { data } from "./data";
 function ButtonsFilter({filterItem, setCard}) {
     return(
         <div className="buttonFilter">
-        <button onClick={ () => setCard(data)}>ALL</button>
+        <button onClick={ () => setCard(data) }>ALL</button>
             <button onClick={ () => filterItem('dress') }>DRESS</button>
             <button onClick={ () => filterItem('shoes') }>SHOES</button>
             <button onClick={ () => filterItem('skirt') }>SKIRT</button>
