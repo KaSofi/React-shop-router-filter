f27e960ace96d25d4a27572b0b96ab372279226a
Mila Gvozdeva <103929930+KaSofi@users.noreply.github.com>
2023-08-04 19:18:56 +0300

    Update AboutProduct.js

--- src/AboutProduct.js
+++ src/AboutProduct.js
@@ -1,10 +1,15 @@
 import { useParams, useNavigate } from "react-router-dom";
 import { data } from "./data";
+import CardImagesCarusel from "./CardImagesCarusel";
+
 
 function AboutProduct() {
 
     const navigate = useNavigate();
     const { title } = useParams();
+    
+
+
 
     return(
         <div>
@@ -12,12 +17,9 @@ function AboutProduct() {
             return (
                 <div key={index} className="AboutProductBlock">
                     <div className="AboutProductPosition">
-                        <div className="AboutProductCarusel">
-                            <button>BACK</button>
-                            <img src={elem.image} alt="clothes" />
-                            <button>NEXT</button>
-                        </div>
                         <h1>{elem.name}</h1>
+                        <img src={elem.image} alt="clothes" />
+                        {/* <CardImagesCarusel/> */}
                         <p>{elem.description}</p>
                         <h2>$ {elem.price}</h2>
                         <button onClick={ () => navigate(-1) }>GO BACK</button>
